166faa0a3340c9b37008dbe19631904b9b9a871a
Egor Dubenetskiy <egor@banka.space>
2019-10-30 22:29:04 +0300

    Fix creating tablespaces

--- 60-create-tablespaces.sh
+++ 60-create-tablespaces.sh
@@ -4,20 +4,17 @@
 
 sqlplus SYS/123456 as SYSDBA <<@
     CREATE TABLESPACE LAST_WHITE_MOM
-          DATAFILE '$ORADATA/$DB_NAME/node02/lastwhitemom01.dbf' SIZE 10M
-        , DATAFILE '$ORADATA/$DB_NAME/node02/lastwhitemom02.dbf' SIZE 10M
-        , DATAFILE '$ORADATA/$DB_NAME/node02/lastwhitemom03.dbf' SIZE 10M
-        ;
+        DATAFILE '$ORADATA/$DB_NAME/node02/lastwhitemom01.dbf' SIZE 10M,
+        DATAFILE '$ORADATA/$DB_NAME/node02/lastwhitemom02.dbf' SIZE 10M,
+        DATAFILE '$ORADATA/$DB_NAME/node02/lastwhitemom03.dbf' SIZE 10M;
     
     CREATE TABLESPACE ILL_ORANGE_OVEN
-          DATAFILE '$ORADATA/$DB_NAME/node01/illorangeoven01.dbf' SIZE 10M
-        , DATAFILE '$ORADATA/$DB_NAME/node03/illorangeoven02.dbf' SIZE 10M
-        ;
+        DATAFILE '$ORADATA/$DB_NAME/node01/illorangeoven01.dbf' SIZE 10M,
+        DATAFILE '$ORADATA/$DB_NAME/node03/illorangeoven02.dbf' SIZE 10M;
     
     CREATE TABLESPACE BEST_YELLOW_MATH
-          DATAFILE '$ORADATA/$DB_NAME/node03/bestyellowmath01.dbf' SIZE 10M
-        , DATAFILE '$ORADATA/$DB_NAME/node02/bestyellowmath02.dbf' SIZE 10M
-        ;
+        DATAFILE '$ORADATA/$DB_NAME/node03/bestyellowmath01.dbf' SIZE 10M,
+        DATAFILE '$ORADATA/$DB_NAME/node02/bestyellowmath02.dbf' SIZE 10M;
     
     /
     EXIT
