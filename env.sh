188cb0e142a9038fbcbddf8e58670549c47f3ee1
Egor Dubenetskiy <egor@banka.space>
2019-10-31 15:57:06 +0300

    Set default lang to Russian

--- env.sh
+++ env.sh
@@ -7,7 +7,7 @@
 export ORACLE_BASE=/u01/app/oracle
 export ORACLE_HOME=$ORACLE_BASE/product/11.2.0/dbhome_1
 export ORACLE_SID=s225088
-export NLS_LANG=American_America.UTF8
+export NLS_LANG=RUSSIAN_CIS.AL32UTF8
 
 export ORADATA=/u01/avy64
 export DB_NAME=loudsoup
