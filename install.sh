9af868af5b18b4f0aa8a6fe7e095998b9bbe7084
Egor Dubenetskiy <egor@banka.space>
2019-10-31 16:12:48 +0300

    Fix paths in install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 source ./env.sh
 
-./20-sys-auth.sh
-./30-init-file.sh
-./40-data-storage.sh
-./50-create-database.sh
-./60-create-tablespaces.sh
-./70-create-dict.sh
+install/20-sys-auth.sh
+install/30-init-file.sh
+install/40-data-storage.sh
+install/50-create-database.sh
+install/60-create-tablespaces.sh
+install/70-create-dict.sh
